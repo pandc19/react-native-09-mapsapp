199eff0500c1a219a1b9dbfc82543cd0cbc1ce91
Jonathan Pillajo <pandc19@outlook.com>
2025-05-25 14:18:41 -0500

    feat: add zustand for state management and implement location permission store

--- src/actions/permissions/location.ts
+++ src/actions/permissions/location.ts
@@ -22,7 +22,7 @@ export const requestLocationPermission =
 
     if (status === 'blocked') {
       await openSettings();
-      // return await // checkLocationPermission();
+      return await checkLocationPermission();
     }
 
     const permissionMapper: Record<RNPermissionStatus, PermissionStatus> = {
