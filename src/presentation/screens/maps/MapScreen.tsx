bf305183946bca5e4065b137a431d752a4bbb988
Jonathan Pillajo <pandc19@outlook.com>
2025-05-25 20:19:14 -0500

    feat: create Map component and integrate it into MapScreen

--- src/presentation/screens/maps/MapScreen.tsx
+++ src/presentation/screens/maps/MapScreen.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
-import {Platform, StyleSheet, View} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {StyleSheet, View} from 'react-native';
+import {Map} from '../../components/maps/Map';
 
 export const MapScreen = () => {
   return (
     <View style={styles.container}>
-      <MapView
-        provider={Platform.OS === 'ios' ? undefined : PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}></MapView>
+      <Map />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  map: {
     ...StyleSheet.absoluteFillObject,
   },
 });
