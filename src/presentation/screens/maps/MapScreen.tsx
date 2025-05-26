b5d3049ae14063eb11035baa18bdf118c131b9eb
Jonathan Pillajo <pandc19@outlook.com>
2025-05-25 20:32:49 -0500

    feat: add geolocation functionality and integrate it into the MapScreen

--- src/presentation/screens/maps/MapScreen.tsx
+++ src/presentation/screens/maps/MapScreen.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Map} from '../../components/maps/Map';
+import {getCurrentLocation} from '../../../actions/location/location';
 
 export const MapScreen = () => {
-  return (
-    <View style={styles.container}>
-      {/* <Map /> */}
-    </View>
-  );
+  return <View style={styles.container}>{/* <Map /> */}</View>;
 };
 
 const styles = StyleSheet.create({
